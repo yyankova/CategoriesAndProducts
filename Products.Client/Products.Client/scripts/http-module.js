b1e07a44c6d71645d5b584b69bd1f2c5bc8d36d6
yyankova <yana.o.yankova@gmail.com>
2015-02-18 13:02:30 +0200

    List products with paging.

--- Products.Client/Products.Client/scripts/http-module.js
+++ Products.Client/Products.Client/scripts/http-module.js
@@ -1,36 +1,27 @@
 ﻿var httpModule = (function httpModule($) {
-    function makeRequest(url, type, data, headers, onSuccess, onError) {
+    function makeRequest(url, type, data, onSuccess, onError) {
         $.ajax(url, {
             type: type,
             data: data,
-            headers: headers,
             success: onSuccess,
             error: onError
             });
     }
     
-    function get(url, headers, onSuccess, onError) {
-        makeRequest(url, 'GET', null, headers, onSuccess, onError);
+    function get(url, onSuccess, onError) {
+        makeRequest(url, 'GET', null, onSuccess, onError);
     }
 
-    function post(url, headers, data, onSuccess, onError) {
-        makeRequest(url, 'POST', data, headers, onSuccess, onError);
+    function post(url, data, onSuccess, onError) {
+        makeRequest(url, 'POST', data, onSuccess, onError);
     }
 
-    function put(url, headers, data, onSuccess, onError) {
-        makeRequest(url, 'PUT', data, headers, onSuccess, onError);
+    function put(url, data, onSuccess, onError) {
+        makeRequest(url, 'PUT', data, onSuccess, onError);
     }
 
-    function del(url, headers, data, onSuccess, onError) {
-        makeRequest(url, 'DELETE', data, headers, onSuccess, onError);
-        //If the upper code does not work
-        //$.ajax(url, {
-        //    type: 'POST',
-        //    data: { _method: 'delete' },
-        //    headers: headers,
-        //    success: onSuccess,
-        //    error: onError
-        //});
+    function del(url, data, onSuccess, onError) {
+        makeRequest(url, 'DELETE', data, onSuccess, onError);
     }
 
     return {
